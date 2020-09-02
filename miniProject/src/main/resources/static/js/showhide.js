57dbdc6a4b9fb33e6dd144d425f717c8efe97862
unknown <phanakornr@pccth.com>
2020-09-02 16:24:56 +0700

    [CREATE] : CREATE Function

--- miniProject/src/main/resources/static/js/showhide.js
+++ miniProject/src/main/resources/static/js/showhide.js
@@ -1,40 +1,9 @@
-
-        function myFunction() {
-        var checkBox = document.getElementById("myCheckboxId");
-        var commerce = document.getElementById("commerce");
-            if (checkBox.checked == true){
-                commerce.style.display = "block";
+function Mycheckbox(checkbox,dv) {
+            var checkbox = document.getElementById(checkbox); 
+            var dv = document.getElementById(dv);     
+            if (checkbox.checked == true){
+                dv.style.display = "block";
             } else {
-                commerce.style.display = "none";
-            }
-        }
-
-        function myFunction1() {
-        var checkBox = document.getElementById("myCheckboxId1");
-        var commerce = document.getElementById("commerce1");
-            if (checkBox.checked == true){
-                commerce.style.display = "block";
-            } else {
-                commerce.style.display = "none";
-            }
-        }
-
-         function myFunction2() {
-        var checkBox = document.getElementById("myCheckboxId2");
-        var commerce = document.getElementById("commerce2");
-            if (checkBox.checked == true){
-                commerce.style.display = "block";
-            } else {
-                commerce.style.display = "none";
-            }
-        }
-
-         function myFunction3() {
-        var checkBox = document.getElementById("myCheckboxId3");
-        var commerce = document.getElementById("commerce3");
-            if (checkBox.checked == true){
-                commerce.style.display = "block";
-            } else {
-                commerce.style.display = "none";
+                dv.style.display = "none";
             }
         }
