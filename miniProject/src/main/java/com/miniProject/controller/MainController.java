3cb6cda1deaecbaac78ede1da4ef4632a648c0be
unknown <phanakornr@pccth.com>
2020-09-10 15:39:25 +0700

    [CREATE]NEW

--- miniProject/src/main/java/com/miniProject/controller/MainController.java
+++ miniProject/src/main/java/com/miniProject/controller/MainController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	 @RequestMapping("/")
+		 @RequestMapping("/")
 	    public String index(Model model) {
 			model.addAttribute("title", "miniProject");
 	        return "Login/Login";
-	    }
+		}
+		
+		@RequestMapping("1")
+	    public String Form(Model model) {
+			model.addAttribute("title", "miniProject");
+	        return "Form/Form";
+	    }	
 }
