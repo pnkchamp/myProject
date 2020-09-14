8c62c7dd93d617520ebf00dcda1e5e8b23250b58
unknown <phanakornr@pccth.com>
2020-09-14 10:21:17 +0700

    [ADD]Function

--- miniProject/src/main/resources/static/js/formcommerce.js
+++ miniProject/src/main/resources/static/js/formcommerce.js
@@ -1,12 +1,14 @@
 
-function showCommerce(checkbox, dv) {
+function showCommerce(checkbox,dv) {
     var checkBox = document.getElementById(checkbox);
     var dv = document.getElementById(dv);
     if (checkBox.checked == true) {
-        dv.classList.remove("commerce");
         dv.classList.add("show");
+        dv.classList.remove("hide");
+
     } else {
-        dv.classList.remove("show");
-        dv.classList.add("commerce");
+        dv.classList.add("hide");
+        dv.classList.remove("show");    
+
     }
 }
